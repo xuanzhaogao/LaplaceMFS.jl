54133938c5aa43ba5a6154b7f9aa7ea905d036fa
Xuanzhao Gao <xgao@flatironinstitute.org>
2026-02-24 14:45:44 -0500

    update

--- src/LaplaceMFS.jl
+++ src/LaplaceMFS.jl
@@ -2,7 +2,11 @@ module LaplaceMFS
 
 using LinearAlgebra, KrylovKit
 using FMM3D
+using LazyArtifacts
 
+export effsphdes_path
+
+effsphdes_path() = joinpath(artifact"EffSphDes", "EffSphDes")
 
 
 end
